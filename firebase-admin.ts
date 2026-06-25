286bbaa4a0147d7e4497e043f39404f92caf5429
MarsalShyam <shyam12marsal@gmail.com>
2026-06-25 18:09:58 +0530

    service-error resolve

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -5,13 +5,16 @@ import{
 import { getFirestore } from "firebase-admin/firestore";
 
 // const serviceKey=require("@/service_key.json")
-let serviceKey;
 
-if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
-  serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
-} else {
-  serviceKey = require("./service_key.json");
-}
+
+// if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+//   serviceKey = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+// } else {
+//   serviceKey = require("./service_key.json");
+// }
+const serviceKey = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY!
+);
 
 let app:App;
 
